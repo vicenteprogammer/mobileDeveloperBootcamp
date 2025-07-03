27740b2d5d457b6ac2ef629457bcb35149853a03
vicenteprogrammer <vicentejunior4522@gmail.com>
2025-07-03 16:05:01 -0300

    new functions

---
+++ mario-kart-simulation/src/index.js
@@ -0,0 +1,25 @@
+const player1 = {
+    name: 'Mario',
+    speed: 4,
+    maneuverability: 3,
+    power: 3,
+    points: 0
+};
+
+const player2 = {
+    name: 'Peach',
+    speed: 3,
+    maneuverability: 4,
+    power: 2,
+    points: 0
+}
+
+
+async function rollDice(){
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+(async function main() {
+    console.log('Hello world')
+})()
+
