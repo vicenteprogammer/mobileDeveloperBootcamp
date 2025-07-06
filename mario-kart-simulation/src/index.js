abcbb024ae0ffda1c7a7ad01e5b9b73fbbfb9fdd
vicenteprogrammer <vicentejunior4522@gmail.com>
2025-07-05 23:11:01 -0300

    function getBlock

--- mario-kart-simulation/src/index.js
+++ mario-kart-simulation/src/index.js
@@ -19,7 +19,35 @@ async function rollDice(){
     return Math.floor(Math.random() * 6) + 1;
 }
 
-async function playRaceEngine(character1, character2) {}
+async function getRandomBlock() {
+    let random = Math.random() 
+
+    let result = '' 
+
+    switch (true){
+        case random < 0.33:
+            result = 'CURVA';
+            break;
+        case random < 0.66:
+            result = 'RETA';
+            break;
+        default:
+            result = 'CONFRONTO';
+            break;
+    }
+
+    return result
+}
+
+async function playRaceEngine(character1, character2) {
+    for(let round = 1; round <= 5; round++){
+        console.log(`🏁 Rodada ${round} começou...`);
+         
+        let block = await getRandomBlock();
+        console.log(`Bloco: ${block}`);
+    }
+
+}
 
 (async function main() {
     console.log(`🏁 🏎️  ${player1.name} e ${player2.name} começaram uma corrida...\n `)
