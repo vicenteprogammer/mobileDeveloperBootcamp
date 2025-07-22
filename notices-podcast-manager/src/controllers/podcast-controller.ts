b4ebe480c8878ac9ca617911f000b9e824d2a582
vicenteprogrammer <vicentejunior4522@gmail.com>
2025-07-22 13:40:04 -0300

    Creat firs service of list episodes

--- notices-podcast-manager/src/controllers/podcast-controller.ts
+++ notices-podcast-manager/src/controllers/podcast-controller.ts
@@ -1,6 +1,7 @@
 import {IncomingMessage, ServerResponse} from 'http';
+import { serviceListEpisodes } from '../services/list-espisodes-service';
 
 export const getListEpisodes = async (req:IncomingMessage, res:ServerResponse)=>{
     res.writeHead(200,{"Content-Type":"application/json"});
-    res.end(JSON.stringify({name:'DepaulaDev'}))
+    res.end(JSON.stringify(await serviceListEpisodes()))
 }
--- notices-podcast-manager/src/server.ts
+++ notices-podcast-manager/src/server.ts
@@ -2,7 +2,9 @@ import * as http from 'http';
 import { getListEpisodes } from './controllers/podcast-controller';
 
 const server =  http.createServer(async (req:http.IncomingMessage, res: http.ServerResponse)=>{
-    await getListEpisodes(req,res);
+    if(req.method === 'GET'){
+        await getListEpisodes(req,res);
+    }
 });
 
 const port = process.env.PORT 
