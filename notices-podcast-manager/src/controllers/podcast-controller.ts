e3210a17c1676ec3e80b88dc0cff2ce9a3654d83
vicenteprogrammer <vicentejunior4522@gmail.com>
2025-07-25 09:54:29 -0300

    Refact Magic numbers and Floating numbers.

--- notices-podcast-manager/src/controllers/podcast-controller.ts
+++ notices-podcast-manager/src/controllers/podcast-controller.ts
@@ -1,16 +1,18 @@
 import {IncomingMessage, ServerResponse} from 'http';
 import { serviceListEpisodes } from '../services/list-espisodes-service';
 import { filterPodcasts } from '../services/filter-podcasts';
+import { StatusCode } from '../utils/status-code';
+import { ContentType } from '../utils/content-type';
 
 export const getListEpisodes = async (req:IncomingMessage, res:ServerResponse)=>{
     const content = await serviceListEpisodes()
-    res.writeHead(200,{"Content-Type":"application/json"});
+    res.writeHead(StatusCode.OK,{"Content-Type":ContentType.JSON});
     res.end(JSON.stringify(content))
 }
 
 export const filterPod  = async(req:IncomingMessage,res:ServerResponse)=>{
   
   const content = await filterPodcasts(req.url)
-  res.writeHead(200, {"Content-Type":"application/json"})
+  res.writeHead(StatusCode.OK, {"Content-Type":"application/json"})
   res.end(JSON.stringify(content))
 }
--- notices-podcast-manager/src/server.ts
+++ notices-podcast-manager/src/server.ts
@@ -1,16 +1,18 @@
 import * as http from 'http';
 import { getListEpisodes, filterPod } from './controllers/podcast-controller';
+import { Routes } from './routes/routes';
+import { HttpMethods } from './utils/http-methods';
 
 const server =  http.createServer(async (req:http.IncomingMessage, res: http.ServerResponse)=>{
 
     const [baseUrl, queryString] = req.url?.split('?') ?? ['',''];
 
 
-    if(req.method === 'GET' && baseUrl === '/api/list'){
+    if(req.method === HttpMethods.GET && baseUrl === Routes.LIST){
         await getListEpisodes(req,res);
     }
 
-    if(req.method === 'GET' && baseUrl === '/api/episode'){
+    if(req.method === HttpMethods.GET && baseUrl === Routes.EPISODES){
         await filterPod(req,res);
     }
 })
