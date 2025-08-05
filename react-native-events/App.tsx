3faa1b793e235ebc27c780cb0b7d63a488b474e3
vicenteprogrammer <vicentejunior4522@gmail.com>
2025-08-05 14:27:48 -0300

    Um pouco do evento onPress

--- react-native-events/App.tsx
+++ react-native-events/App.tsx
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const greet = () =>{
+    Alert.alert('Olá, usúario')
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{marginBottom: 30}} >Open up App.tsx to start working on your app!</Text>
+      {/*onPress é um dos principais eventos que compõem vários componentes de react native, muito usado em botões e alguns textos específicos, nesse evento podemos passar funções e paramêtros para a função que será executado, assim como no javascript */}
+
+      <Button title='Greeting' onPress={greet}/>
       <StatusBar style="auto" />
     </View>
   );
