ef4dd6ae9b9f0e466f55373b0975de63531f5703
vicenteprogrammer <vicentejunior4522@gmail.com>
2025-08-11 08:41:39 -0300

    Adicionando icones a bootom tabs

--- react-navigation/src/routes/bottom.routes.tsx
+++ react-navigation/src/routes/bottom.routes.tsx
@@ -1,6 +1,6 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 import HomeScreen from '../screens/HomeScreen';
-
+import {MaterialCommunityIcons} from '@expo/vector-icons'
 const {Screen, Navigator} = createBottomTabNavigator();
 
 export default function BottomRoutes(){
@@ -9,10 +9,27 @@ export default function BottomRoutes(){
         <Screen
          name='home'
          component={HomeScreen}
+         options={{
+            tabBarIcon:({color,size})=>(
+                <MaterialCommunityIcons
+                 name='home'
+                 size={24}
+                 color={'blue'}
+                />
+            )
+         }}
         />
         <Screen
          name='gallery'
          component={HomeScreen}
+         options={{
+            tabBarIcon:({color, size})=>(
+                <MaterialCommunityIcons
+                 name="view-gallery"
+                 size={24}
+                 color="blue" />
+            )
+         }}
         />
     </Navigator>
     )
